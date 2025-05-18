ee54aaaba92200102f70f64463917f9b8617eba4
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-05-18 15:13:47 +0100

    functions included
    added seller_below_avg_items and seller_statistics functions. the seller_statistics shows the min, max and average product price of a seller and seller_below_avg_items shows a seller's items below their average selling price

--- customers.sql
+++ customers.sql
@@ -50,19 +50,65 @@ WHERE to_tsvector(name) @@ to_tsquery('(baby ) &  (!coat)');
 -- subquery 
 -- find the items greater than the average price of products
 
-
-
+-- find the total number of listing per seller
 WITH seller_details AS (
    SELECT 
-		CONCAT (c.firstname, ' ', c.lastname) seller,
-		COUNT(i.item_id) item_count
+		CONCAT (firstname, ' ',lastname) seller,
+		COUNT(item_id) item_count
 	FROM items i
 	JOIN customers c ON c.customer_id = i.seller_id
 	GROUP BY seller 
-	HAVING COUNT(i.item_id) <3
+	ORDER BY seller, item_count 
 )
+SELECT * FROM seller_details;
 
-SELECT seller, item_count
-FROM seller_details
-ORDER BY seller, item_count 
+-- find items less than the average price of all products
+WITH avg_items_cost AS (SELECT AVG(cost) avg_cost FROM items)
+
+SELECT name, pounds(cost) price, pounds(avg_cost) average_price
+FROM items, avg_items_cost
+WHERE cost > avg_cost
+ORDER BY price 
+-- a function that finds the sellar statistics
+CREATE OR REPLACE FUNCTION seller_statistics(sellers_id INTEGER)
+   RETURNS TABLE (
+        min_cost money,
+        max_cost money,
+		avg_cost money
+) 
+AS 
+$$
+   SELECT
+   pounds(MIN(cost)),
+   pounds(MAX(cost)), 
+   pounds(AVG(cost)) 
+   FROM items 
+   WHERE seller_id = sellers_id;
+$$
+LANGUAGE sql;
+
+-- find a seller's item below their average selling price:
+CREATE OR REPLACE FUNCTION seller_below_avg_items(sellers_id INTEGER)
+   RETURNS TABLE (
+        product_name VARCHAR,
+        item_price money
+) 
+AS 
+$$
+	   WITH avg_items_cost AS
+	(SELECT AVG(cost) avg_cost FROM items WHERE seller_id = sellers_id)
+	
+	SELECT name product_name, pounds(cost) item_price
+	FROM items, avg_items_cost
+	WHERE seller_id = sellers_id 
+	 AND cost < avg_cost
+	GROUP BY name, cost;
+$$
+LANGUAGE sql;
+
+
+SELECT * FROM seller_below_avg_items(24); 
+SELECT * FROM seller_statistics(24);
+
+
 
